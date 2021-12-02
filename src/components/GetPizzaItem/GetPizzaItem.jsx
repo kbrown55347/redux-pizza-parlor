0fedd13b2ec3a589eff2eaaa420ff1834efb13f5
Nicholas Ilacqua <Niservices88@gmail.com>
2021-12-02 16:30:34 -0600

    Dcreated a GetPizzaItem.jsx component

--- src/components/GetPizzaItem/GetPizzaItem.jsx
+++ src/components/GetPizzaItem/GetPizzaItem.jsx
@@ -1,12 +1,12 @@
-
+import './GetPizzaItem.css'
 function GetPizzaItem({ pizza }){
     return(
-        <>
-            <> {pizza.name} 
-               {pizza.description}
-               {pizza.price} </>
-            <img src={pizza.image_path}/>
-        </>
+        <div>
+            <div>{pizza.name} </div>
+            <div>{pizza.description}</div>
+            <div>{pizza.price} </div>
+            <img className="img"src={pizza.image_path}/>
+        </div >
     );
 };
 
