3c9d556d26a4a32f33b2cddaabff4182efa4f316
Kayla Mir <kayla.mir32@gmail.com>
2021-12-03 10:31:23 -0600

    checkout post worked on, waiting for master update

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from "react-router-dom";
+
+import CheckoutHome from './CheckoutHome/CheckoutHome';
 
 function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-  
+      <Router>
+        <header className='App-header'>
+          <h1 className='App-title'>Prime Pizza</h1>
+        </header>
+        <Link to="/">Select Pizza</Link>|
+        <Link to="/checkout">Checkout</Link>
+{/*     
+        <img src='images/pizza_photo.png' />
+        <p>Pizza is great.</p> */}
+        <Route exact path="/checkout">
+          <CheckoutHome />
+        </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/components/App/CheckoutHome/CheckoutHome.jsx
@@ -0,0 +1,89 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import CheckoutItem from "../../CheckoutItem/CheckoutItem";
+
+function CheckoutHome() {
+    // call the order holder reducer that has the stored information
+    // change names when order reducer is named IMPORTANT
+    // const orderReducer = useSelector((store) => store.orderReducer);
+    const history = useHistory();
+    
+    const testOrder = {
+        customer_name: "Donatello",
+        street_address: "20 W 34th St",
+        city: "New York",
+        zip: "10001",
+        total: "27.98",
+        type: "Pickup",
+        pizzas: [{
+            id: "1",
+            quantity: "1"
+        }, {
+            id: "2",
+            quantity: "1"
+        }]
+    }
+    // function to post data to the database
+    // clears the cart
+    // navigates back to pizza page
+    const handleCheckout = () => {
+        // const newOrder = {
+        //     customer_name: orderReducer.customer_name, //change values when reducer is made
+        //     street_address: orderReducer.street_address,
+        //     city: orderReducer.city,
+        //     zip: orderReducer.zip,
+        //     total: orderReducer.total,
+        //     type: orderReducer.type,
+        //     pizzas: orderReducer.pizzas
+        // }
+        axios({
+            method: 'POST',
+            url: '/api/order',
+            data: testOrder
+        }).then((res) => {
+            //   dispatchEvent({
+            //       type: '',
+            //       payload: res.data
+            //   })
+            alert('Thank you for your order!');
+            history.push('/');
+        }).catch((err) => {
+            console.error('POST checkout error', err)
+        })
+    }
+
+    return (
+        <div>
+            <h3>Step 3: Checkout</h3>
+            <div id="customerInfo">
+                <h4>For {testOrder.type}</h4>
+                <>{testOrder.customer_name}</><br />
+                <>{testOrder.street_address}</><br />
+                <>{testOrder.city}, {testOrder.zip}</>
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Cost</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {testOrder.pizzas.map((pizza, i) => {
+                            return <CheckoutItem key={i} pizza={pizza.id}/>
+                        })}   
+                    </tbody>
+                </table>
+            </div>
+            <div id="total-checkout">
+                <h2>Total: ${testOrder.total}</h2>
+                <button onClick={handleCheckout}>CHECKOUT</button>
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutHome;
